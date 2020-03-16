c6fa09de32d5513f278ec6a433fc5776d82466a5
BenLue <BenJule@users.noreply.github.com>
2020-03-16 13:58:01 +0100

    Update reposync.sh

--- reposync.sh
+++ reposync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2020 los-legacy.de, All rights reserved
 # Mitch, Exodusnick, BenLue
-#set -xe
+set +xe
 cd "${SYSTEM_PATH}" || exit
 export PATH=~/bin:$PATH
 git config --global user.name 'Jenkins'
