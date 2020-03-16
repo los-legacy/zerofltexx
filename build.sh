78b31aadc5a62e1d9f9f48b5cbd28d30959e3876
BenLue <BenJule@users.noreply.github.com>
2020-03-16 12:58:22 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright 2020 los-legacy.de, All rights reserved
+# Mitch, Exodusnick, BenLue
+set +xe
+cd "${SYSTEM_PATH}" || exit
+export PATH=~/bin:$PATH
+#make clean
+source build/envsetup.sh
+export USE_CCACHE=1
+ccache -M 50G
+export CCACHE_COMPRESS=1
+#breakfast "${DEVICE}"
+#brunch "${DEVICE}" 
