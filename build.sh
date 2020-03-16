c355384d87afdb2c83eb3c60b42332e8bafba441
BenLue <BenJule@users.noreply.github.com>
2020-03-16 15:53:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 set +xe
 cd "${SYSTEM_PATH}" || exit
 export PATH=~/bin:$PATH
-make clean
+#make clean
 source build/envsetup.sh
 export USE_CCACHE=1
 ccache -M 50G
 export CCACHE_COMPRESS=1
-breakfast "${DEVICE}"; brunch "${DEVICE}"
+#breakfast "${DEVICE}"; brunch "${DEVICE}"
